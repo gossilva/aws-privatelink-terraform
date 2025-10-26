4c6d73b2cdea2286d80c6715254daf3ae25d680e
unknown <guilherme.derf@gmail.com>
2025-10-26 01:01:14 -0300

    feat(ssm): store VPC Endpoint Service name in SSM Parameter for cross-VPC access

---
+++ vpc1/modules/network/service-ssm-parameter.tf
@@ -0,0 +1,11 @@
+resource "aws_ssm_parameter" "vpc1_ssm_service_name" {
+  name  = "/privatelink/service_name"
+  type  = "String"
+  value = aws_vpc_endpoint_service.vpc1_vpce_service.service_name
+  tags = merge(
+    var.tags,
+    {
+      Name = "${var.project_name}-ssm-service-name"
+    }
+  )
+}
