f87251d53827997165a00821e2c9e0347ad1bf56
unknown <guilherme.derf@gmail.com>
2025-10-26 01:30:06 -0300

    feat(vars): define core variables for modules

---
+++ vpc2/variables.tf
@@ -0,0 +1,9 @@
+variable "cidr_block" {
+  type        = string
+  description = "CIDR block value to create VPC."
+}
+
+variable "project_name" {
+  type        = string
+  description = "Default naming convention for vpc2 resources"
+}
