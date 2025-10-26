e3f5aa43ba92c9c085e2660c55c08cd067c3f0d5
unknown <guilherme.derf@gmail.com>
2025-10-26 01:25:49 -0300

    feat(ec2): implement EC2 instance with user data and security groups in compute module

---
+++ vpc2/modules/compute/ec2.tf
@@ -0,0 +1,13 @@
+resource "aws_instance" "vpc2_ec2" {
+  subnet_id              = var.vpc2_private_subnet_1a
+  ami                    = "ami-0341d95f75f311023"
+  instance_type          = "t3.micro"
+  iam_instance_profile   = aws_iam_instance_profile.vpc2_ec2_instance_profile.name 
+  tags = merge(
+    var.tags,
+    {
+      Name = "${var.project_name}-ec2"
+    }
+  )
+}
+
