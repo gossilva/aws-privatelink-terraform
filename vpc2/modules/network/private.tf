022acf0a325b1c98d76eb1a9e559f68dd9c2b637
unknown <guilherme.derf@gmail.com>
2025-10-26 01:18:00 -0300

    feat(private-subnet): implement private subnets and routing associations

---
+++ vpc2/modules/network/private.tf
@@ -0,0 +1,35 @@
+resource "aws_subnet" "vpc2_private_subnet_1a" {
+  vpc_id            = aws_vpc.vpc2_vpc.id
+  cidr_block        = cidrsubnet(var.cidr_block, 8, 3)
+  availability_zone = "${data.aws_region.current.region}a"
+
+  tags = merge(
+    var.tags,
+    {
+      Name = "${var.project_name}-private-subnet-1a"
+    }
+  )
+}
+
+resource "aws_subnet" "vpc2_private_subnet_1b" {
+  vpc_id            = aws_vpc.vpc2_vpc.id
+  cidr_block        = cidrsubnet(var.cidr_block, 8, 4)
+  availability_zone = "${data.aws_region.current.region}b"
+
+  tags = merge(
+    var.tags,
+    {
+      Name = "${var.project_name}-private-subnet-1b"
+    }
+  )
+}
+
+resource "aws_route_table_association" "vpc2_private_rtb_assoc_1a" {
+  subnet_id      = aws_subnet.vpc2_private_subnet_1a.id
+  route_table_id = aws_route_table.vpc2_private_rtb_1a.id
+}
+
+resource "aws_route_table_association" "vpc2_private_rtb_assoc_1b" {
+  subnet_id      = aws_subnet.vpc2_private_subnet_1b.id
+  route_table_id = aws_route_table.vpc2_private_rtb_1b.id
+}
